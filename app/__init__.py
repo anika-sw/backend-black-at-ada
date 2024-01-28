6b9173d3cda8d0cd22fe59d21de451614ba6fe02
Anika Stephen Wilbur <anika.m.stephen@gmail>
2024-01-28 03:24:33 -0800

    Change to how Database URL is fetched

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 import requests
 import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
