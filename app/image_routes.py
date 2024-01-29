489f031608012bdea8b6120ba7d0d56a99fda38f
Anika Stephen Wilbur <anika.m.stephen@gmail>
2024-01-28 21:06:19 -0800

    Add image post key to heroku config vars

--- app/image_routes.py
+++ app/image_routes.py
@@ -15,7 +15,7 @@ image_bp = Blueprint("images", __name__, url_prefix="/images")
 def upload_image_to_cloud_storage():
     uploaded_file = request.files.get("image")
     org_filename = uploaded_file.filename
-    storage_client = storage.Client.from_service_account_json("image_upload_sa.json")
+    storage_client = storage.Client.from_service_account_json(os.environ.get("image_upload_sa.json"))
     bucket_name = os.environ.get("GCP_STORAGE_BUCKET_NAME")
     bucket = storage_client.bucket(bucket_name)
     file_extension = org_filename.split('.').pop()
